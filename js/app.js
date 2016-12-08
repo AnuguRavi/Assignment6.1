e12f84c23c8941e1df7859eec714a975a999f655
Ravi Anugu <ravi.rgu09@gmail.com>
2016-12-08 21:22:13 +0530

    Add files via upload
    Operators

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var radius = 10; //assign the radius
+var area; //declaring the variable area
+
+area = Math.PI * (radius ** 2); //calculating the area using the formula 
+
+console.log("Area of the circle is : ",area); //Printing the area of the circle.
